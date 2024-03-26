9aef553fab56deaf513227d5adf5beaa3dbfa6f9
jtumidanski <jtumidanski@users.noreply.github.com>
2024-03-26 11:02:02 -0400

    Compress background video.

--- web/src/app.ts
+++ web/src/app.ts
@@ -5,23 +5,10 @@ import {customElement} from 'lit/decorators.js';
 @customElement('app-root')
 export class App extends LitElement {
   static styles = css`
-      #video-background {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100vw;
-          height: 100vh;
-          object-fit: cover;
-          z-index: -1;
-      }
   `;
 
   render() {
     return html`
-      <video id="video-background" autoplay muted loop>
-        <source src="/videos/background-video.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-      </video>
       <div id="outlet"></div>
     `;
   }
