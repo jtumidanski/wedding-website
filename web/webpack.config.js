ed0da4f5fb7a4439bc38a5646d11d94afce613b0
jtumidanski <jtumidanski@users.noreply.github.com>
2024-03-26 15:50:21 -0400

    Clean-up double entrypoint. Build Lit for prod.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,10 +2,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
